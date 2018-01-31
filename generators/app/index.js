fd5b09faf5bb8d61dd3795e391b6d096933c8d41
Frederick-S <mao_xiaodan@hotmail.com>
2018-01-31 18:26:33 +0800

    Update prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,10 +11,18 @@ module.exports = class extends Generator {
     ));
 
     const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'groupId',
+      message: 'GroupId'
+    }, {
+      type: 'input',
+      name: 'artifactId',
+      message: 'ArtifactId'
+    }, {
+      type: 'input',
+      name: 'version',
+      message: 'Version',
+      default: '1.0-SNAPSHOT'
     }];
 
     return this.prompt(prompts).then(props => {
@@ -28,9 +36,5 @@ module.exports = class extends Generator {
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
     );
-  }
-
-  install() {
-    this.installDependencies();
   }
 };
