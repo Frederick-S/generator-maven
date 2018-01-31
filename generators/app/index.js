c8cf9c38dd3a721117c51ec92ec504a1d5ac6039
Frederick-S <mao_xiaodan@hotmail.com>
2018-01-31 19:32:46 +0800

    Use const

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs-extra');
-var path = require('path');
+const path = require('path');
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
