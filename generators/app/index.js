9b0bd81cf1c89165e402a411d51e18838ab3413a
Frederick-S <mao_xiaodan@hotmail.com>
2018-02-01 12:42:25 +0800

    Update test

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,24 +9,28 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the primo ' + chalk.red('generator-maven') + ' generator!'
-    ));
+    this.log(
+      yosay('Welcome to the primo ' + chalk.red('generator-maven') + ' generator!')
+    );
 
-    const prompts = [{
-      type: 'input',
-      name: 'groupId',
-      message: 'GroupId'
-    }, {
-      type: 'input',
-      name: 'artifactId',
-      message: 'ArtifactId'
-    }, {
-      type: 'input',
-      name: 'version',
-      message: 'Version',
-      default: '1.0-SNAPSHOT'
-    }];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'groupId',
+        message: 'GroupId'
+      },
+      {
+        type: 'input',
+        name: 'artifactId',
+        message: 'ArtifactId'
+      },
+      {
+        type: 'input',
+        name: 'version',
+        message: 'Version',
+        default: '1.0-SNAPSHOT'
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -35,24 +39,20 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('pom.xml'),
-      this.destinationPath('pom.xml'),
-      {
-        groupId: this.props.groupId,
-        artifactId: this.props.artifactId,
-        version: this.props.version
-      }
-    )
+    this.fs.copyTpl(this.templatePath('pom.xml'), this.destinationPath('pom.xml'), {
+      groupId: this.props.groupId,
+      artifactId: this.props.artifactId,
+      version: this.props.version
+    });
 
-    const folders = ['src/main/java', 'src/main/resources', 'src/test/java']
+    const folders = ['src/main/java', 'src/main/resources', 'src/test/java'];
 
     for (let i = 0; i < folders.length; i++) {
-      fs.ensureDir(path.join(this.destinationRoot(), folders[i]), (error) => {
+      fs.ensureDir(path.join(this.destinationRoot(), folders[i]), error => {
         if (error) {
           console.log(error);
         }
-      })
+      });
     }
   }
 };
